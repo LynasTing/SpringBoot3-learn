5aba293d52ebba25f814c82e0b9eff2826d2225e
LynasTing <LynasTing@proton.me>
2024-05-23 16:05:58 +0800

    -ci: springboot工程手动创建

---
+++ springboot-create-manual/src/main/java/com/lynas/SpringBootCreateManualApplication.java
@@ -0,0 +1,15 @@
+package com.lynas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Hello world!
+ */
+@SpringBootApplication
+public class SpringBootCreateManualApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootCreateManualApplication.class, args);
+    System.out.println("Hello World!");
+  }
+}
